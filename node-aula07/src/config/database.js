4f6ef6f84a1c0e80bca063c81c840e78f96c3092
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-04-04 09:37:23 -0400

    aula 7 - feat: configura projeto com sequelize

---
+++ node-aula07/src/config/database.js
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+
+// config do sqlite
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: false,
+});
+
+export default sequelize;
---
+++ node-aula07/src/model/Pessoa.js
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+
+class Pessoa extends Model {}
+
+Pessoa.init(
+    {
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false // campo obrigatorio
+        },
+        
+        telefone: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Pessoa',
+        tableName: 'pessoas',
+        timestamps: true
+    }
+);
+
+export default Pessoa;

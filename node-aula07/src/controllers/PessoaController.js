c2b0e95255aaa78b0204440fa29d1d91c0c52ad0
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-04-04 10:56:35 -0400

    aula 7 - feat: cria endpoint post para criar pessoa e instala nodemon

--- node-aula07/src/controllers/PessoaController.js
+++ node-aula07/src/controllers/PessoaController.js
@@ -1,5 +1,6 @@
 import Pessoa from "../model/Pessoa.js";
 
+// listar pessoas
 export const listarPessoas = async(req, res) => {
     try{
         const pessoas = await Pessoa.findAll();
@@ -8,4 +9,15 @@ export const listarPessoas = async(req, res) => {
     catch(error){
         res.status(500).json({ error: "Erro ao listar pessoas" });
     }
-}
+};
+
+// criar pessoa
+export const criarPessoa = async(req, res) => {
+    try{
+        const novaPessoa = await Pessoa.create(req.body);
+        res.status(201).json(novaPessoa);
+    }
+    catch(error) {
+        res.status(400).json({error: "Erro ao criar pessoa.", detalhes: error});
+    }
+};
--- node-aula07/src/routes/PessoaRoutes.js
+++ node-aula07/src/routes/PessoaRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
-    listarPessoas
+    listarPessoas,
+    criarPessoa
 } from "../controllers/PessoaController.js";
 
 const router = express.Router();
 
 // rotas para pessoas
 router.get('/', listarPessoas); // lista todas as pessoas
+router.post('/', criarPessoa); // cria pessoa
 // localhost:3000/pessoas
 
 export default router;
